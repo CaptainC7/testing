f6ff56de39995b749ab4a48bcf871c295472065b
Taha Mardini <tahamardini62726@gmail.com>
2024-09-06 16:33:27 +0300

    Add project files.

---
+++ testing/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace testing.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string FName { get; set; } = "";
+        public string LName { get; set; } = "";
+        public string Gender { get; set; } = "";
+        public string BDate { get; set; } = "";
+        public string Username { get; set; } = "";
+        public string Password { get; set; } = "";
+    }
+}
