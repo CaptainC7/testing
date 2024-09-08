bf98abe10974018eb2700f346f432589edaf83d3
Taha Mardini <tahamardini62726@gmail.com>
2024-09-09 00:36:48 +0300

    everything

--- testing/Program.cs
+++ testing/Program.cs
@@ -1,11 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using ClassLibraryDLL.Models.ApplicationDBContext;
+using ClassLibraryDLL.Services;
+using ClassLibraryDLL.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IPersonServices, PersonServices>(); // If it implements an interface
+//builder.Services.AddDbContext<dbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServerDb")));
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServerDb")));
 
 var app = builder.Build();
 
